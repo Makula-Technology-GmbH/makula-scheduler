a271320fe708832db471b7df8b2abc0d85be6958
Muhammad Bilal <mac@macs-MacBook-Pro.local>
2026-02-25 15:35:22 +0500

    more cleanup

--- src/examples/app.jsx
+++ src/examples/app.jsx
@@ -1,9 +1,7 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
 import './css/style.css';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <></>;
 }
 
 export default App;
